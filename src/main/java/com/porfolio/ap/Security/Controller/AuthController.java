a48916210150211b41f3a85f75e29675dc1b3409
Mpablom <pablomoya211@gmail.com>
2023-04-24 19:31:02 -0300

    prueba de deploy

--- src/main/java/com/porfolio/ap/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/ap/Security/Controller/AuthController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontapp-64aae.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
