40765167504886fc600a497ef33d34721e914830
Mpablom <pablomoya211@gmail.com>
2023-04-25 19:09:51 -0300

    pruebas para lograr deployar

--- src/main/java/com/porfolio/ap/Controller/CExperiencia.java
+++ src/main/java/com/porfolio/ap/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontapp-64aae.web.app/")
+@CrossOrigin(origins = {"https://frontapp-64aae.web.app/","https://frontapp-64aae.firebaseapp.com/","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
